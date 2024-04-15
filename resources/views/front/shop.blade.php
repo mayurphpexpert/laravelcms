551f70dfec7e94a9de223a37407996234cf05b22
mayurphpexpert <mayur@indiaphpexpert.com>
2024-04-15 11:09:42 +0530

    Create Some New Module - Both side second time 15/04/24

--- resources/views/front/shop.blade.php
+++ resources/views/front/shop.blade.php
@@ -138,7 +138,7 @@
                                             </div>
                                         </div>
                                         <div class="card-body text-center mt-3">
-                                            <a class="h6 link" href="product.php">{{ $product->title }}</a>
+                                            <a class="h6 link" href="{{ route('front.product',$product->slug) }}">{{ $product->title }}</a>
                                             <div class="price mt-2">
                                                 <span class="h5"><strong>₹{{ $product->price }}</strong></span>
                                                 @if ($product->compare_price > 0)
